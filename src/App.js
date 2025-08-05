9533332846c246759bfadb207811adcbd56d31ce
Andrey Aheienko <ageenko01114444@gmail.com>
2025-08-05 18:07:30 +0300

    push Git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import AboutSection from './components/AboutSection';
+import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import ImageSection from './components/ImageSection';
+import PricingSection from './components/PricingSection';
+import ResultsSection from './components/ResultsSection';
+import ServisesSection from './components/ServisesSection';
+import './index.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <ServisesSection />
+        <ResultsSection/>
+        <PricingSection/>
+        <ImageSection/>
+        <ContactSection/>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
